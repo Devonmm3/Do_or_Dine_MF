0aceef84fc4443eb164fbab8bfa2d85f495656c9
DevonMM <Devon.m.moran@gmail.com>
2018-12-15 10:50:06 -0500

    beginning stuff

---
+++ pseudocode.js
@@ -0,0 +1,16 @@
+//6 clickable photos/objects.  each is a different cuisine.
+//var: model (name ?)
+//{name: chinese, american, mexican, vegan, indian, japanese}
+//need event click for each photo, which will hide the other (not-clicked) photos, and produce link options for restaurants and recipes.
+
+//the api's will have a set parameter to fetch  5 results for restaurants and 5 for recipes.
+//the results produced for restaurants will come from the API (yelp) parameter based on 
+//restaurant location, rating, name, dollar sign and price range
+//the results produced from recipe api (food2fork) based on 
+//recipe title, rating, image, link to full recipe and ingrediente (?)
+
+//the onclick function will be a for loop, running through each of the objects
+
+
+//need a config var, it will come from the database created through firebase.  both API keys also need to be stored there
+//need need to intialize the app {var database = firebase.database()}
