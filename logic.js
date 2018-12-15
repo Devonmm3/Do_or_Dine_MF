beb8760087f52b49b06cbea77620e228f32458d5
Kmayer48 <kenny.mayer3@gmail.com>
2018-12-15 11:10:05 -0500

    recipe API function

---
+++ logic.js
@@ -0,0 +1,9 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript"></script>
+
+$(document).on("click", ".image-card", function() {
+    var url = "https://www.food2fork.com/api/search"
+    var API = "fb4f39eb5ea6ffbeb0f3f3811432fc39"
+    var q = 
+    var 
+}
